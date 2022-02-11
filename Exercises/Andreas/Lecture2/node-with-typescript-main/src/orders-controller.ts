5a94aa7dbb7ffa573d6d6333ed36a65c8f0ead17
AndreasV <201705136@post.au.dk>
2022-02-11 11:22:24 +0100

    Endpoints finished, but not tested

--- Exercises/Andreas/Lecture2/node-with-typescript-main/src/orders-controller.ts
+++ Exercises/Andreas/Lecture2/node-with-typescript-main/src/orders-controller.ts
@@ -1,30 +1,74 @@
 import { Request, Response } from 'express'
 import mongoose from 'mongoose'
 
-const transactionsConnection = mongoose.createConnection('mongodb://localhost:27017/orders')
+import { schema } from './order'
+
+
+const orderConnection = mongoose.createConnection('mongodb://localhost:27017/orders')
+const orderModel = orderConnection.model('Order', schema)
+
 
 const list = async (req: Request, res: Response) => {
-    res.json("Orders listed");
+    const { src, dst, f, t } = req.query
+
+    let filter = {}
+
+    if(src) {
+        filter = {src}
+    }
+
+    if(dst){
+        filter = {...filter, dst}
+    }
+
+    if(f && t){
+        filter = {...filter, ts: {$gt: f, $lt: t}}
+    } else{
+        if(f){
+            filter = {...filter, ts: {$gt: f}}
+        }
+        if(t){
+            filter = {...filter, ts: {$lt: t}}
+        }
+    }
+
+    let result = await orderModel.find(filter, { __v: 0 }).lean()
+    res.json(result);
 }
 
 const create =  async (req: Request, res: Response) => {
-    res.json("ID Created")
+    let { id } = await new orderModel(req.body).save()
+    res.json({ id } + " is the ID of your created order.")
 }
 
 const read = async (req: Request, res: Response) => {
-    res.json("Here is a single order with an ID")
+    const { uid } = req.params
+    let result = await orderModel.find({ _id: uid}, {__v: 0}).exec()
+    res.json(result)
 }
 
 const overwrite = async (req: Request, res:Response) => {
-      res.json("You have now PUT an order")
+      const { uid } = req.params
+      const body = req.body
+      let result = orderModel.findOne({_id: uid}, {__v: 0}).exec()
+      if(result){
+          let resp = result.overwrite(body)
+          res.json(resp)
+      } else {
+          res.sendStatus(404)
+      }
 }
 
 const update = async (req: Request, res:Response) => {
-    res.json("You have now PATCH an order")
+    const {uid} = req.params
+    let result = orderModel.updateOne({_id: uid}, {$set: {amnt: 100, src: '123', dst: '321'}}).exec()
+    res.json({uid, result})
 }
 
 const remove = async (req: Request, res:Response) => {
-    res.json("You have now deleted an order")
+    const { uid } = req.params
+    let result = await orderModel.deleteOne({ _id: uid })
+    res.json(result)
 }
 
 export const Orders = {
