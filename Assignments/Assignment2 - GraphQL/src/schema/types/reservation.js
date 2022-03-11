40c1f76bda78a80bacb271bee93ad9cc44cf155e
NOVICELL\tnb <tnb@novicell.dk>
2022-03-11 10:58:50 +0100

    added to reservation

--- Assignments/Assignment2 - GraphQL/src/schema/types/reservation.js
+++ Assignments/Assignment2 - GraphQL/src/schema/types/reservation.js
@@ -3,6 +3,7 @@ import {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
+    GraphQLFloat,
     GraphQLNonNull,
     printSchema,
     GraphQLBoolean,
@@ -16,8 +17,14 @@ import {
   const Reservation = new GraphQLObjectType({
     name: "Room",
     fields:{
-      ReservationNumber: {type: new GraphQLNonNull(GraphQLInt)},
-      fromDate: {type: new GraphQLNonNull(DateTimeTypeDefinition)}
+      reservationNumber: {type: new GraphQLNonNull(GraphQLInt)},
+      fromDate: {type: new GraphQLNonNull(DateTimeTypeDefinition)},
+      toDate: {type: new GraphQLNonNull(DateTimeTypeDefinition)},
+      nameOfReserver: {type: new GraphQLNonNull(GraphQLString)},
+      emailOfReserver: {type: new GraphQLNonNull(GraphQLString)},
+      commentsOfReserver: {type: new GraphQLNonNull(GraphQLString)},
+      discount: {type: new GraphQLNonNull(GraphQLBoolean)},
+      totalMoneySaved: {type: new GraphQLNonNull(GraphQLFloat)}
     }
   });
 
