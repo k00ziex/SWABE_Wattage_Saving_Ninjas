247ccf5df962e77d6d532b71de38de1d30ec6d3f
tobiassandoelund <tobiassandoelund@gmail.com>
2022-03-11 09:43:07 +0100

    Runs

--- Assignments/Assignment2 - GraphQL/src/db/pg-api.js
+++ Assignments/Assignment2 - GraphQL/src/db/pg-api.js
@@ -1,5 +1,4 @@
 import pgClient from './pg-client';
-import { randomString } from '../utils';
 
 const pgApiWrapper = async () => {
     const { pgPool } = await pgClient();
@@ -42,33 +41,8 @@ const pgApiWrapper = async () => {
             return pgResp.rows;
         },
         mutators: {
-            userCreate: async ({ input }) => {
-              const payload = { errors: [] };
-              if (input.password.length < 6) {
-                payload.errors.push({
-                  message: 'Use a stronger password',
-                });
-              }
-              if (payload.errors.length === 0) {
-                const authToken = randomString();
-                const pgResp = await pgQuery( `
-                INSERT INTO azdev.users (username, hashed_password, first_name, last_name, hashed_auth_token)
-                VALUES ($1, crypt($2, gen_salt('bf')), $3, $4, crypt($5, gen_salt('bf')))
-                RETURNING id, username, first_name AS "firstName", last_name AS "lastName", created_at AS "createdAt"
-              `, {
-                  $1: input.username.toLowerCase(),
-                  $2: input.password,
-                  $3: input.firstName,
-                  $4: input.lastName,
-                  $5: authToken,
-                });
-                if (pgResp.rows[0]) {
-                  payload.user = pgResp.rows[0];
-                  payload.authToken = authToken;
-                }
-              }
-              return payload;
-            },
+            
+          
         },
     };
 };
