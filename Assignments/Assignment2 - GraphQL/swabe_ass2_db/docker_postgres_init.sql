3aa54cc522cb4c901f332651bc187899f4d47548
tobiassandoelund <tobiassandoelund@gmail.com>
2022-03-18 11:51:37 +0100

    Add room uid to reservation

--- Assignments/Assignment2 - GraphQL/swabe_ass2_db/docker_postgres_init.sql
+++ Assignments/Assignment2 - GraphQL/swabe_ass2_db/docker_postgres_init.sql
@@ -16,15 +16,16 @@ CREATE TABLE Rooms
 INSERT INTO 
 Rooms(uid, roomNumber, available, comment, floor, bedAmount, bedType, roomServiceAvailable, soundProof, hasOwnTub)
 VALUES
-('1', 420, TRUE, 'No dogs allowed', 'Ground Floor', 1, 'King', FALSE, TRUE, TRUE),
-('2', 421, FALSE, 'Dogs allowed', 'Second Floor', 2, 'Normal', TRUE, FALSE, FALSE),
-('3', 423, TRUE, 'Smoking Allowed', 'Basement', 4, 'King', TRUE, TRUE, TRUE);
+('1', 420, FALSE, 'No dogs allowed', 'Ground Floor', 1, 'King', FALSE, TRUE, TRUE),
+('2', 421, TRUE, 'Dogs allowed', 'Second Floor', 2, 'Normal', TRUE, FALSE, FALSE),
+('3', 423, FALSE, 'Smoking Allowed', 'Basement', 4, 'King', TRUE, TRUE, TRUE);
 
 
 
 CREATE TABLE Reservations
 (
 	uid VARCHAR(255) UNIQUE NOT NULL,
+	roomuid VARCHAR(255) NOT NULL,
     fromdate VARCHAR(255) NOT NULL,
     todate VARCHAR(255) NOT NULL,
     nameofreserver VARCHAR(255) NOT NULL,
@@ -33,10 +34,10 @@ CREATE TABLE Reservations
 );
 
 INSERT INTO 
-Reservations(uid, fromdate, todate, nameofreserver, emailofreserver, comments)
+Reservations(uid, roomuid, fromdate, todate, nameofreserver, emailofreserver, comments)
 VALUES
-('1', '2022-01-01 12:00:00', '2022-01-05 11:00:00', 'Andreas Vorgaard', 'andy@gmail.com', 'I would like a noose pre-hung please'),
-('2', '2022-01-01 10:00:00', '2022-01-05 24:00:00', 'Tobias Lund', 'tobias@gmail.com', 'Chokolade på puden tak'),
-('3', '2022-01-01 08:00:00', '2022-01-05 17:00:00', 'Thomas Berthel', 'thomas@gmail.com', 'Hvad skal jeg skrive her?') 
+('1', '1', '2022-01-01 12:00:00', '2022-01-05 11:00:00', 'Andreas Vorgaard', 'andy@gmail.com', 'I would like a noose pre-hung please'),
+('2', '1', '2022-05-01 12:00:00', '2022-01-10 24:00:00', 'Tobias Lund', 'tobias@gmail.com', 'Chokolade på puden tak'),
+('3', '3', '2022-01-01 08:00:00', '2022-01-05 17:00:00', 'Thomas Berthel', 'thomas@gmail.com', 'Hvad skal jeg skrive her?') 
 
 
