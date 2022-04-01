dba794416fc8b9886cc338c0ba50f0f1b634b497
NOVICELL\tnb <tnb@novicell.dk>
2022-04-01 11:30:06 +0200

    added const

---
+++ Assignments/Assignment3 - RabbitMQ/reservation_handler/Reservation.Handler/Reservation.Handler/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Reservation.Handler
+{
+    public class Constants
+    {
+        public const string Hotel_Room_Topic = "hotel_room_reservation_topic";
+        public const string Hotel_Room_Queue = "hotel_room_reservation_queue";
+        public const string Reservation_Received_Topic = "reservation_topic";
+        public const string Reservation_Received_Queue = "reservation_queue";
+    }
+}
