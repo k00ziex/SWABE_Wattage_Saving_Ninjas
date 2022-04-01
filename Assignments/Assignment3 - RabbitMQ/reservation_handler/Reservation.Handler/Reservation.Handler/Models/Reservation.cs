e09f02b00bd61fe44b481ef18d4ab2a74372278a
NOVICELL\tnb <tnb@novicell.dk>
2022-04-01 11:12:56 +0200

    added initial stuff

---
+++ Assignments/Assignment3 - RabbitMQ/reservation_handler/Reservation.Handler/Reservation.Handler/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿namespace Reservation.Handler.Models
+{
+    public class Reservation
+    {
+        public Reservation()
+        {
+        }
+
+        public int ReservationNumber { get; set; }
+        public int HotelId { get; set; }
+        public string? CheckIn { get; set; }
+        public string? CheckOut { get; set; }
+        public int RoomNo { get; set; }
+        public string? CustomerName { get; set; }
+        public string? CustomerEmail { get; set; }
+        public string? CustomerAddress { get; set; }
+    }
+}
