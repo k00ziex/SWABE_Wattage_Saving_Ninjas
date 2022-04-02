774517a915cdef83d412d04e553eeea1ea79ab33
DESKTOP-55PV0K9\berth <berthel0802@gmail.com>
2022-04-02 19:43:10 +0200

    fuk nullable

--- Assignments/Assignment3 - RabbitMQ/reservation_handler/Reservation.Handler/Reservation.Handler/DatabaseContext/ReservationContext.cs
+++ Assignments/Assignment3 - RabbitMQ/reservation_handler/Reservation.Handler/Reservation.Handler/DatabaseContext/ReservationContext.cs
@@ -5,9 +5,7 @@ namespace Reservation.Handler.DatabaseContext
 {
     public class ReservationContext : DbContext
     {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public ReservationContext(DbContextOptions<ReservationContext> options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
             : base(options) { }
 
         public DbSet<Models.Reservation> Reservations { get; set; }
