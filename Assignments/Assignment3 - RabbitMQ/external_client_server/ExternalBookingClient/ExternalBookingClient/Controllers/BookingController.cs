a274d04d1acbfb5a02c7738e299fc2758c4d2cd1
AndreasV <201705136@post.au.dk>
2022-04-08 11:06:33 +0200

    Persistent messages & input validation

--- Assignments/Assignment3 - RabbitMQ/external_client_server/ExternalBookingClient/ExternalBookingClient/Controllers/BookingController.cs
+++ Assignments/Assignment3 - RabbitMQ/external_client_server/ExternalBookingClient/ExternalBookingClient/Controllers/BookingController.cs
@@ -49,15 +49,21 @@ namespace ExternalBookingClient.Controllers
 
         private static string ValidateInput(BookingInput input)
         {
-            if (!DateTime.TryParseExact(input.CheckIn, Iso8601Format, CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+            if (!DateTime.TryParseExact(input.CheckIn, Iso8601Format, CultureInfo.InvariantCulture, DateTimeStyles.None, out var checkInDate))
             {
                 var errorMessage = $"{nameof(input.CheckIn)} must be a valid date in accordance with the ISO 8601 standard of 'yyyy-MM-ddTHH:mm:ss.fffZ'.";
                 return errorMessage;
             }
 
-            if (!DateTime.TryParseExact(input.CheckOut, Iso8601Format, CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+            if (!DateTime.TryParseExact(input.CheckOut, Iso8601Format, CultureInfo.InvariantCulture, DateTimeStyles.None, out var checkOutDate))
             {
                 var errorMessage = $"{nameof(input.CheckOut)} must be a valid date in accordance with the ISO 8601 standard of 'yyyy-MM-ddTHH:mm:ss.fffZ'.";
+                return errorMessage;
+            }
+
+            if (DateTime.Compare(checkInDate.Date, checkOutDate.Date) >= 0)
+            {
+                var errorMessage = $"The {nameof(input.CheckOut)} date must a later date than the {nameof(input.CheckIn)}.";
                 return errorMessage;
             }
 
